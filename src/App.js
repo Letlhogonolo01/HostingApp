6ade8b80749af41eba255784b0dfaa3e621ae2af
Letlhogonolo01 <letlhogonolo.moroke8@gmail.com>
2023-08-18 11:06:33 +0200

    added an input field

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [name, setName] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("My name is " + name);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hosting Tester
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hosting Tester</h1>
+      <form onSubmit={handleSubmit}>
+        <div>What is your name?</div>
+        <input
+          type="text"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+        <input type="submit" />
+      </form>
     </div>
   );
 }
