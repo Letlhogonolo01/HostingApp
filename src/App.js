ad0b67e99903f3b9419291ddcaba245e7172ea88
Letlhogonolo01 <letlhogonolo.moroke8@gmail.com>
2023-08-18 15:01:26 +0200

    updates

--- src/App.js
+++ src/App.js
@@ -1,40 +1,40 @@
 // import logo from "./logo.svg";
 import "./App.css";
-// import { useState } from "react";
+import { useState } from "react";
 
 function App() {
-  // const [name, setName] = useState("");
+  const [name, setName] = useState("");
 
-  // const handleSubmit = (event) => {
-  //   event.preventDefault();
-  //   alert("My name is " + name);
-  // };
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert("My name is " + name);
+  };
 
   return (
     <div className="App">
       <h1>Hosting Tester</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <div>Todo</div>
         <input
           type="text"
-          // value={name}
-          // onChange={(e) => setName(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input type="submit" />
       </form>
       <form>
       <input
           type="text"
-          // value={name}
-          // onChange={(e) => setName(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input type="submit" />
         </form>
         <form>
       <input
           type="text"
-          // value={name}
-          // onChange={(e) => setName(e.target.value)}
+          value={name}
+          onChange={(e) => setName(e.target.value)}
         />
         <input type="submit" />
         </form>
